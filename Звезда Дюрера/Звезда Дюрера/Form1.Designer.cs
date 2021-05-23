cf96a67043f4d90e7288a03cd0f9648e0a4c7cee
Alex <tseplyaeff.alex@yandex.ru>
2021-05-23 10:29:14 +0900

    + homework: threads, gui

---
+++ Звезда Дюрера/Звезда Дюрера/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace Звезда_Дюрера
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button_Draw = new System.Windows.Forms.Button();
+            this.textBox_Angle = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_Radius = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button_SetAngle = new System.Windows.Forms.Button();
+            this.button_SetRadius = new System.Windows.Forms.Button();
+            this.label_Angle = new System.Windows.Forms.Label();
+            this.label_Radius = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(648, 435);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button_Draw
+            // 
+            this.button_Draw.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_Draw.Location = new System.Drawing.Point(522, 482);
+            this.button_Draw.Name = "button_Draw";
+            this.button_Draw.Size = new System.Drawing.Size(120, 38);
+            this.button_Draw.TabIndex = 1;
+            this.button_Draw.Text = "Нарисовать";
+            this.button_Draw.UseVisualStyleBackColor = true;
+            this.button_Draw.Click += new System.EventHandler(this.button_Draw_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox_Angle.Location = new System.Drawing.Point(94, 466);
+            this.textBox_Angle.Name = "textBox1";
+            this.textBox_Angle.Size = new System.Drawing.Size(100, 20);
+            this.textBox_Angle.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 469);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Угол поворота:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 495);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Радиус:";
+            // 
+            // textBox_Radius
+            // 
+            this.textBox_Radius.Location = new System.Drawing.Point(94, 492);
+            this.textBox_Radius.Name = "textBox_Radius";
+            this.textBox_Radius.Size = new System.Drawing.Size(100, 20);
+            this.textBox_Radius.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(3, 439);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Параметры фрактала:";
+            // 
+            // button_SetAngle
+            // 
+            this.button_SetAngle.Location = new System.Drawing.Point(200, 464);
+            this.button_SetAngle.Name = "button_SetAngle";
+            this.button_SetAngle.Size = new System.Drawing.Size(75, 23);
+            this.button_SetAngle.TabIndex = 11;
+            this.button_SetAngle.Text = "Задать";
+            this.button_SetAngle.UseVisualStyleBackColor = true;
+            this.button_SetAngle.Click += new System.EventHandler(this.button_SetAngle_Click);
+            // 
+            // button_SetRadius
+            // 
+            this.button_SetRadius.Location = new System.Drawing.Point(200, 490);
+            this.button_SetRadius.Name = "button_SetRadius";
+            this.button_SetRadius.Size = new System.Drawing.Size(75, 23);
+            this.button_SetRadius.TabIndex = 12;
+            this.button_SetRadius.Text = "Задать";
+            this.button_SetRadius.UseVisualStyleBackColor = true;
+            this.button_SetRadius.Click += new System.EventHandler(this.button_SetRadius_Click);
+            // 
+            // label_Angle
+            // 
+            this.label_Angle.AutoSize = true;
+            this.label_Angle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_Angle.Location = new System.Drawing.Point(519, 439);
+            this.label_Angle.Name = "label_Angle";
+            this.label_Angle.Size = new System.Drawing.Size(45, 16);
+            this.label_Angle.TabIndex = 13;
+            this.label_Angle.Text = "Угол: ";
+            // 
+            // label_Radius
+            // 
+            this.label_Radius.AutoSize = true;
+            this.label_Radius.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_Radius.Location = new System.Drawing.Point(519, 458);
+            this.label_Radius.Name = "label_Radius";
+            this.label_Radius.Size = new System.Drawing.Size(62, 16);
+            this.label_Radius.TabIndex = 14;
+            this.label_Radius.Text = "Радиус: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 528);
+            this.Controls.Add(this.label_Radius);
+            this.Controls.Add(this.label_Angle);
+            this.Controls.Add(this.button_SetRadius);
+            this.Controls.Add(this.button_SetAngle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_Radius);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_Angle);
+            this.Controls.Add(this.button_Draw);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Звезда Дюрера";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_Draw;
+        private System.Windows.Forms.TextBox textBox_Angle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_Radius;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button_SetAngle;
+        private System.Windows.Forms.Button button_SetRadius;
+        private System.Windows.Forms.Label label_Angle;
+        private System.Windows.Forms.Label label_Radius;
+    }
+}
+
---
+++ Звезда Дюрера/Звезда Дюрера/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Звезда_Дюрера
+{
+    public partial class Form1 : Form
+    {
+        Pen p;
+        SolidBrush backgroundColor;
+        Graphics gr;
+        static double pi = 3.14;
+        double angle = pi / 2;
+        int radius = 85;
+
+        public Form1()
+        {
+            InitializeComponent();
+            label_Angle.Text += "90";
+            label_Radius.Text += "85";
+        }
+
+        void Draw_Pentagon(double x, double y, double r, double angle)
+        {
+            p = new Pen(Color.Blue, 2);
+            gr = pictureBox1.CreateGraphics();
+            gr.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            int i;
+            double[] x1 = new double[5];
+            double[] y1 = new double[5];
+
+            for (i = 0; i < 5; i++)
+            {
+                x1[i] = r * Math.Cos(angle + i * pi * 2 / 5);
+                y1[i] = r * Math.Sin(angle + i * pi * 2 / 5);
+            }
+
+            for (i = 0; i < 4; i++)
+            {
+                gr.DrawLine(p, (int)Math.Round(x + x1[i]), (int)Math.Round(y + y1[i]), (int)Math.Round(x + x1[i + 1]), (int)Math.Round(y + y1[i + 1]));
+            }
+        }
+
+        void Draw_Star(double x, double y, double r, double angle, int d)
+        {
+            int i;
+            double h;
+
+            h = 2 * r * Math.Cos(pi / 5);
+
+            for (i = 0; i < 5; i++)
+            {
+                Draw_Pentagon(x - h * Math.Cos(angle + i * pi * 2 / 5), y - h * Math.Sin(angle + i * pi * 2 / 5), r, angle + pi + i * pi * 2 / 5);
+
+                if (d > 0)
+                    Draw_Star(x - h * Math.Cos(angle + i * pi * 2 / 5), y - h * Math.Sin(angle + i * pi * 2 / 5), r / (2 * Math.Cos(pi / 5) + 1), angle + pi + (2 * i + 1) * pi * 2 / 10, d - 1);
+            }
+            Draw_Pentagon(x, y, r, angle);
+
+            if (d > 0)
+                Draw_Star(x, y, r / (2 * Math.Cos(pi / 5) + 1), angle + pi, d - 1);
+        }
+
+        private void button_Draw_Click(object sender, EventArgs e)
+        {
+            backgroundColor = new SolidBrush(BackColor);
+            gr = pictureBox1.CreateGraphics();
+            gr.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            gr.FillRectangle(backgroundColor, 0, 0, pictureBox1.Width, pictureBox1.Height);
+            //button_Draw.Enabled = false;
+
+            Thread Thread_Drawing = new Thread(
+                // Делегат - ссылка на метод
+                delegate ()
+                {
+                    Draw_Star(300, 250, radius, angle, 4);
+                });
+            Thread_Drawing.Start();
+
+            //await Task.Run(() =>
+            //{
+            //    Draw_Star(300, 250, radius, angle, 4);
+            //});
+
+            //button_Draw.Enabled = true;
+        }
+
+        double DegreesToRadian(double angle) => angle * pi / 180;
+
+        private void NewAngle()
+        {
+            label_Angle.Text = "Угол: ";
+            angle = Convert.ToInt32(textBox_Angle.Text);
+            label_Angle.Text += angle.ToString() + " °";
+            angle = DegreesToRadian(angle);
+        }
+
+        private void NewRadius()
+        {
+            label_Radius.Text = "Радиус: ";
+            radius = Convert.ToInt32(textBox_Radius.Text);
+            label_Radius.Text += radius.ToString();
+        }
+
+        private void button_SetAngle_Click(object sender, EventArgs e)
+        {
+            NewAngle();
+        }
+
+        private void button_SetRadius_Click(object sender, EventArgs e)
+        {
+            NewRadius();
+        }
+    }
+}
